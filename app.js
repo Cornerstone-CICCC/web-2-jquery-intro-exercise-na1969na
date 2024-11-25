6d08d672224728d63d28f40fdaab27c696fb2dc1
na1969na <nakmtn07@gmail.com>
2024-11-25 11:45:13 -0800

    Add exercises

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-$(function() {
-  // YOUR CODE HERE
-})
+$(function () {
+  $("#toggleButton").click(function () {
+    $("#myParagraph").toggle();
+  });
+
+  $('#colorButton').click(function() {
+    $('#colorDiv').css('background-color', 'yellow');
+  });
+
+  $('#addClassButton').click(function() {
+    $('#classDiv').addClass('newClass');
+  });
+  $('#removeClassButton').click(function() {
+    $('#classDiv').removeClass('newClass');
+  });
+
+  $('#fadeInButton').click(function() {
+    $('#fadeDiv').fadeIn();
+  });
+  $('#fadeOutButton').click(function() {
+    $('#fadeDiv').fadeOut();
+  });
+
+  $('#slideUpButton').click(function() {
+    $('#slideDiv').slideUp();
+  });
+  $('#slideDownButton').click(function() {
+    $('#slideDiv').slideDown();
+  });
+});
